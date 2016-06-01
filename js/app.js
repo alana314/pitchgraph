e613749164fe3fb364121434f4a5ddd7ceeb5e14
jordan314 <jordan314@yahoo.com>
2016-05-31 23:15:32 -0700

    threshold line

--- js/app.js
+++ js/app.js
@@ -38,7 +38,7 @@ $(document).ready(function(){
 
   anim = setInterval(scrollLeft, 20);
 
-  thresholdline = new fabric.Path('M 0 0 L 0 ' + canvasWidth + 'z');
+  thresholdline = new fabric.Path('M 0 0 L ' + canvasWidth +  ' 0 z');
   thresholdline.set({top: 220 + 100, left: 0, fill: '#999', stroke: '#999'});
   group.add(thresholdline);
 });
